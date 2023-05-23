bd1ad96cf1b97e0c83de62d87bd1f83809ea716a
KraxPL <kraxpl@gmail.com>
2023-05-23 17:19:57 +0200

    Category entity is created with its repository and service HomeController moved to final destination

--- src/main/java/pl/krax/charity/controller/HomeController.java
+++ src/main/java/pl/krax/charity/controller/HomeController.java
@@ -1,4 +1,4 @@
-package pl.krax.charity;
+package pl.krax.charity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ src/main/java/pl/krax/charity/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,47 @@
+package pl.krax.charity.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.krax.charity.entities.Category;
+import pl.krax.charity.repo.CategoryRepo;
+import pl.krax.charity.service.CategoryService;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+    private final CategoryRepo categoryRepo;
+
+    @Override
+    @Transactional
+    public void create(Category category) {
+        categoryRepo.save(category);
+    }
+
+    @Override
+    @Transactional
+    public void update(Category category) {
+        categoryRepo.save(category);
+    }
+
+    @Override
+    @Transactional
+    public List<Category> findAll() {
+        return categoryRepo.findAll();
+    }
+
+    @Override
+    @Transactional
+    public Optional<Category> findById(Long categoryId) {
+        return categoryRepo.findById(categoryId);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long categoryId) {
+        findById(categoryId).ifPresent(categoryRepo::delete);
+    }
+}
